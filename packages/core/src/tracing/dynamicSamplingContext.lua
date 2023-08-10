4cb02ea9aea98b15d381cb279a15ff2c425a9dec
Brooke <brooke@gril.me>
2023-08-10 19:57:49 +0100

    feat: Translate `core` dynamicSamplingContext

---
+++ packages/core/src/tracing/dynamicSamplingContext.lua
@@ -0,0 +1,44 @@
+local Types = require("@packages/types")
+type Client = Types.Client
+type DynamicSamplingContext = Types.DynamicSamplingContext
+type Scope = Types.Scope
+type DsnComponents = Types.DsnComponents
+type User = Types.User
+
+local Constants = require("../constants")
+local DEFAULT_ENVIRONMENT = Constants.DEFAULT_ENVIRONMENT
+
+local DynamicSamplingContext = {}
+
+--- Creates a dynamic sampling context from a client.
+---
+--- Dispatches the `createDsc` lifecycle hook as a side effect.
+function DynamicSamplingContext.getDynamicSamplingContextFromClient(
+    trace_id: string,
+    client: Client,
+    scope: Scope?
+): DynamicSamplingContext
+    local options = client:getOptions()
+
+    local dsn = client:getDsn() or {} :: DsnComponents
+    local publicKey = dsn.publicKey
+
+    local user = (scope and scope:getUser()) or {} :: User
+    local segment = user.segment
+
+    local dsc = {
+        environment = options.environment or DEFAULT_ENVIRONMENT,
+        release = options.release,
+        user_segment = segment,
+        public_key = publicKey,
+        trace_id = trace_id,
+    } :: DynamicSamplingContext
+
+    if client.emit then
+        (client.emit :: any)("createDsc", dsc)
+    end
+
+    return dsc
+end
+
+return DynamicSamplingContext
