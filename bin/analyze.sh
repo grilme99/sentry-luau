18515d3f30601334c979ca2e4b0f88941d3fffd6
Brooke <brooke@gril.me>
2025-07-05 02:42:46 +0100

    ci: fix analysis

--- bin/analyze.sh
+++ bin/analyze.sh
@@ -1,4 +1,6 @@
 curl -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
 rojo sourcemap test.project.json > sourcemap.json
 
-luau-lsp analyze --sourcemap sourcemap.json --platform roblox --settings .vscode/settings.json modules/
+luau-lsp analyze --sourcemap sourcemap.json --platform roblox \
+    --definitions globalTypes.d.lua --settings .vscode/settings.json \
+    --ignore=DevPackages/** --ignore=Packages/** modules/
