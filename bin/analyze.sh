f5d8c3e1862a3c8d5848408e7a19d8bc37015f8d
Brooke <brooke@gril.me>
2025-07-05 02:20:11 +0100

    ci: update to latest tools and add test runner

---
+++ bin/analyze.sh
@@ -0,0 +1,4 @@
+curl -O https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua
+rojo sourcemap test.project.json > sourcemap.json
+
+luau-lsp analyze --sourcemap sourcemap.json --platform roblox --settings .vscode/settings.json
